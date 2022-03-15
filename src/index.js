43737052b8d72a595716fd4619635f1e0cfa2439
Jose Pach <jose.pacheco@proagrica.com>
2022-03-14 23:17:09 -0600

    Progress: first HOC

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 // import reportWebVitals from './reportWebVitals';
+
+function App(props) {
+  return (
+    <h1>
+      {props.saludo}, {props.nombre}!
+    </h1>
+  );
+}
+
+function withSaludo(saludo) {
+  return function WrappedComponent(WrappedComponent) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrappedComponent</p>
+        </React.Fragment>
+      );
+    };
+  };
+}
+
+const apiSaludo = withSaludo("Wenitas")(App);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <apiSaludo nombre="Juanito" />
   </React.StrictMode>,
   document.getElementById("root")
 );
